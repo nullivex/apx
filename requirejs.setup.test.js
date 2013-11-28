bd0cb93fd5fde9fce6a7f8693abfcb3b3a5bb4e4
Tony Butler <tony@esited.com>
2013-11-28 05:03:19 -0500

    Poking tests around, they don't work yet...

---
+++ requirejs.setup.test.js
@@ -0,0 +1,31 @@
+/* globals requirejs: false */
+"use strict";
+var tests = []
+for(var file in window.__karma__.files){
+  if(window.__karma__.files.hasOwnProperty(file)){
+    if(/\.spec\.js$/.test(file)){
+      tests.push(file)
+    }
+  }
+}
+
+requirejs.config({
+  // Karma serves files from "/base"
+  baseUrl: "/base",
+
+  paths: {
+    "apex": "apex"
+  },
+
+  shim: {
+    "apex": {
+      exports: "apex"
+    }
+  },
+
+  // ask Require.js to load these files (all our tests)
+  deps: tests,
+
+  // start test run, once Require.js is done
+  callback: window.__karma__.start
+})
--- util/bcrypt.spec.js
+++ util/bcrypt.spec.js
@@ -1,13 +1,10 @@
 "use strict";
-describe("SnailJS.Apex.Util.Bcrypt",function(){
-  var apex = {
-    util: {
-      bcrypt: require("./bcrypt")
-    }
-  }
-  // load the app
-  beforeEach(apex.util.bcrypt.init(12))
-  it("should return proper output when encode(payload)",function(){
-    expect(apex.util.bcrypt.encode("test-string.1.2.3.4_huehuehue")).toEqual("shit")
+define(["apex"],function(apex){
+  describe("SnailJS.Apex.Util.Bcrypt",function(){
+    // load the app
+    beforeEach(apex.util.bcrypt.init(12))
+    it("should return proper output when encode(payload)",function(){
+      expect(apex.util.bcrypt.encode("test-string.1.2.3.4_huehuehue")).toEqual("shit")
+    })
   })
 })

c4f2503df6a16afb0ce16bec490daceeda79bf0a
Bryan Tong <contact@nullivex.com>
2014-01-08 17:23:01 -0700

    tests are passing file object is built

--- test/File.test.js
+++ test/File.test.js
@@ -1,15 +1,26 @@
 'use strict';
 var expect = require('chai').expect
   , File = require('../lib/File')
+  , temp = require('temp')
   , fs = require('fs')
+
 describe('APX File',function(){
-  var file
+  var tmpFile, file
   before(function(){
-    //create a file
-    var file = new File('/tmp/path')
+    //create a temp file
+    tmpFile = temp.openSync()
+    fs.write(tmpFile.fd,'foo')
+    //create a file object
+    file = new File(tmpFile.path)
+  })
+  after(function(){
+    temp.cleanup()
   })
   it('should set the path on construction',function(){
-
-    expect(file.path).to.equal('/tmp/path')
+    expect(file.path).to.equal(tmpFile.path)
+  })
+  it('should have stats populated about the file',function(){
+    expect(file.stats.size).to.equal(3)
+    expect(file.stats.mode).to.equal(33206)
   })
 })
--- test/Response.test.js
+++ test/Response.test.js
@@ -11,33 +11,19 @@ describe('APX Response',function(){
       res.add('my string')
       expect(res.body).to.equal('my string')
     })
-    it('should accept an object to add to the body',function(){
-      res.add({mydata: 'val1'})
+    it('should accept an object to add to the object',function(){
+      res.load({mydata: 'val1'})
       expect(res.get('mydata')).to.equal('val1')
     })
     it('should accept an array of objects to add to the body',function(){
-      res.add([{mydata: 'val1'},{mydata2: 'val2'}])
+      res.load([{mydata: 'val1'},{mydata2: 'val2'}])
       expect(res.get('mydata')).to.equal('val1')
       expect(res.get('mydata2')).to.equal('val2')
     })
-    it('should send and fire the callback',function(){
-      res.send('foo')
-      expect(res.rendered).to.equal('foo')
-    })
     it('should send JSON of the data object',function(){
-      res.send({mydata: 'val1'})
-      var obj = JSON.parse(res.rendered)
+      res.load({mydata: 'val1'})
+      var obj = JSON.parse(JSON.stringify(res.get()))
       expect(obj.mydata).to.equal('val1')
-    })
-    it('should send body for a string response',function(){
-      res.send('foo')
-      expect(res.rendered).to.equal('foo')
-    })
-    it('should send JSON + body if both were set by throw a warning',function(){
-      res.add({mydata: 'val1'})
-      res.send('foo')
-      expect(res.warnings[0]).to.equal('JSON and String body rendered')
-      expect(res.rendered).to.equal('{"mydata":"val1"}foo')
     })
   })
 

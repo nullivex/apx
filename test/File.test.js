e0a89eb42fca9389bc7d5f00987a53fae3d8c7be
Bryan Tong <contact@nullivex.com>
2014-01-20 23:12:54 -0700

    added fileName and fieldName to file object constructor

--- test/File.test.js
+++ test/File.test.js
@@ -4,6 +4,44 @@ var expect = require('chai').expect
   , temp = require('temp')
   , fs = require('fs')
 
+describe('Apx File Constructor',function(){
+  var tmpFile
+  before(function(){
+    tmpFile = temp.openSync()
+  })
+  after(function(){
+    temp.cleanup()
+  })
+  it('should take only the path as an argument',function(){
+    var file = new File(tmpFile.path)
+    expect(file.path).to.not.equal(null)
+  })
+  it('should take a path and a filename',function(){
+    var file = new File(tmpFile.path,'foo.txt')
+    expect(file.name).to.equal('foo.txt')
+  })
+  it('should take a path and an options object',function(){
+    var file = new File(tmpFile.path,{tmpFile: true})
+    expect(file.options.get('tmpFile')).to.equal(true)
+  })
+  it('should take a path, fileName, and fieldName',function(){
+    var file = new File(tmpFile.path,'foo.txt','attachment')
+    expect(file.name).to.equal('foo.txt')
+    expect(file.fieldName).to.equal('attachment')
+  })
+  it('should take a path, fileName, and options object',function(){
+    var file = new File(tmpFile.path,'foo.txt',{tmpFile: true})
+    expect(file.name).to.equal('foo.txt')
+    expect(file.options.get('tmpFile')).to.equal(true)
+  })
+  it('should take a path, fileName, fieldName and options object',function(){
+    var file = new File(tmpFile.path,'foo.txt','attachment',{tmpFile: true})
+    expect(file.name).to.equal('foo.txt')
+    expect(file.fieldName).to.equal('attachment')
+    expect(file.options.get('tmpFile')).to.equal(true)
+  })
+})
+
 describe('APX File',function(){
   var tmpFile, file
   before(function(done){
@@ -11,7 +49,7 @@ describe('APX File',function(){
     tmpFile = temp.openSync()
     fs.writeSync(tmpFile.fd,'foo')
     //create a file object
-    file = new File(tmpFile.path)
+    file = new File(tmpFile.path,'foo.txt','attachment')
     file.populate(done)
   })
   after(function(){
@@ -28,6 +66,10 @@ describe('APX File',function(){
   })
   it('should have an extension based on the mime type',function(){
     expect(file.mimeExtension).to.equal('txt')
+  })
+  it('should support fileName and fieldName being passed',function(){
+    expect(file.name).to.equal('foo.txt')
+    expect(file.fieldName).to.equal('attachment')
   })
   it('should support clearing and repopulating file info',function(){
     expect(file.mimeType).to.equal('text/plain')
